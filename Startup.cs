478bc1053608f6dc79d081808d9ceee5b8d4a3fa
saber13812002 <saber.tabatabaee@gmail.com>
2019-06-23 19:17:57 +0430

    ok tested

--- Startup.cs
+++ Startup.cs
@@ -41,8 +41,11 @@ namespace StuInfo1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
         {
+            dbContext.Database.EnsureCreated();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
